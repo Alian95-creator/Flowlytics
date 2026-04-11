bc5b7e4b65f040a6746bcee642c8c1afb13af91b
Alifian <alifiancandra93@gmail.com>
2026-04-11 23:54:00 +0700

    feat: protect routes with authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
+import { useAuth } from "./hooks/useAuth";
 
 export default function App() {
+  const { user } = useAuth();
+
+  if (!user) return <Login />;
+
   return (
-    <div className="flex bg-gray-100 min-h-screen">
-      <Sidebar />
-      <div className="flex-1">
-        <Header />
-        <div className="p-6">
-          <h1 className="text-2xl font-semibold">Dashboard</h1>
+    <BrowserRouter>
+      <div className="flex bg-gray-100 min-h-screen">
+        <Sidebar />
+        <div className="flex-1">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/users" element={<Users />} />
+          </Routes>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }

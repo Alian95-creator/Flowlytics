f54b4281635e11d85d8f18a6a45038019e6def83
Alifian <alifiancandra93@gmail.com>
2026-04-11 20:53:19 +0700

    style: refine layout spacing and background

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
+    <div className="flex bg-gray-100 min-h-screen">
     <h1 className="text-4xl text-blue-500 font-bold">
       Tailwind Works 🚀
     </h1>
+    </div>
   );
 }

3250e664be6ade83b029a7a1e6924d86367a0979
Alifian <alifiancandra93@gmail.com>
2026-04-12 20:31:25 +0700

    fix: bypass coingecko CORS with proxy

--- src/pages/Crypto.tsx
+++ src/pages/Crypto.tsx
@@ -45,8 +45,11 @@ export default function Crypto() {
 
   async function fetchCoins() {
     const res = await fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=15&page=1&sparkline=true"
-    );
+      "https://api.allorigins.win/raw?url=" +
+        encodeURIComponent(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=15&page=1&sparkline=true"
+    )
+ );
     const data = await res.json();
     setCoins(data);
     setFiltered(data);

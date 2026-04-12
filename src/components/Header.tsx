5449f2f6286483517da79aa27b24e1f617d48cf8
Alifian <alifiancandra93@gmail.com>
2026-04-12 15:19:53 +0700

    fix: remove unused useEffect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { supabase } from "../lib/supabase";
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 type Props = {
   onMenuClick?: () => void;
